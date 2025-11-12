a29632f8e4b7faa8e3fca81024f1cbe893cf5385
ivanhi0 <idbod@ivanhi0.ru>
2025-11-12 21:44:29 +0300

    Now it should work

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ const countrcode = document.getElementById('country_code');
 fetch('https://ipapi.co/json/')
   .then(res => res.json())
   .then(res => {
-    countrcode.textcontext = res.country_code;
+    const countryCode = res.country_code;
+
+    // Redirect user based on his country location because YouTube bloked in Russia
+    if (countryCode == "RU" || countryCode == "RUS") {
+      window.location.href = "https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb";
+      window.location.replace("https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb");
+    } else {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
   });
-
-// Redirect user based on his country location because YouTube bloked in Russia
-if (countrcode.textcontext == "RU") {
-  window.location.href = "https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb";
-  window.location.replace("https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb");
-} else {
-  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-}
-

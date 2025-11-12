69c0e579f19810e8099dd978714c89e287c57ea0
ivanhi0 <idbod@ivanhi0.ru>
2025-11-12 21:58:14 +0300

    Last was working but bloked ru ips

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 //determinate where user located
 const countrcode = document.getElementById('country_code');
 
-fetch('https://ipapi.co/json/')
+fetch('https://ipwho.is/')
   .then(res => res.json())
   .then(res => {
     const countryCode = res.country_code;
 
     // Redirect user based on his country location because YouTube bloked in Russia
-    if (countryCode == "RU" || countryCode == "RUS") {
+    if (countryCode == "RU") {
       window.location.href = "https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb";
       window.location.replace("https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb");
     } else {

63808e22432da301f1991c54cb41bc686c0780c5
ivanhi0 <idbod@ivanhi0.ru>
2025-11-12 21:14:16 +0300

    YouTube isn't working in russia so try to fix it

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // hahaha I got you
-window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+//determinate where user located
+const countrcode = document.getElementById('countryCode');
+
+fetch('http://ip-api.com/json/?fields=61439')
+  .then(res => res.json())
+  .then(res => {
+    countrcode.textcontext = res.countryCode;
+  });
+
+// Redirect user based on his country location because YouTube bloked in Russia
+if (countrcode.textcontext == "RU") {
+  window.location.href = "https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb";
+  window.location.replace("https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb");
+} else {
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+

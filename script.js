74384c8b7dd5c90c374a4f58889d25dbbdba2b7c
ivanhi0 <idbod@ivanhi0.ru>
2026-07-20 17:47:06 +0300

    Update redirect

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ fetch('https://ipwho.is/')
     const countryCode = res.country_code;
 
     // Redirect user based on his country location because YouTube bloked in Russia
+    // Two methods in case one wont work
     if (countryCode == "RU") {
       window.location.href = "https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb";
       window.location.replace("https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb");
     } else {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    window.location.href = "https://www.youtube.com/watch?v=5SZYz7lZRRI";
+    window.location.replace("https://www.youtube.com/watch?v=5SZYz7lZRRI");
     }
   });
